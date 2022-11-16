1d092e2c28d698149dfba3d911626ee0697e0231
Ibrahim <ibrachar68@gmail.com>
2022-11-16 15:10:31 +0100

    fine selezione difficoltà

--- main.js
+++ main.js
@@ -2,31 +2,80 @@ let button = document.getElementById("btn");
 
  button.addEventListener("click",
  function(){
-    let main=document.querySelector("main")
+    let difficolta = document.getElementById("diff").value;
+    let main = document.querySelector("main")
     main.style.backgroundColor = "aqua";
 
     let container = document.getElementById("container");
 
-     for(i = 1; i <= 100; i++ ){
-         let elemento = nuovoElemento();
-          elemento.innerHTML = i;
-          container.append(elemento);
-          
-          elemento.addEventListener("click",
-          function(){
-             elemento.classList.toggle("color")
-          }
-         
-          );
-         
-     }  
+    if (difficolta === "Facile"){
+        document.getElementById("container").innerHTML = ""
+        for(i = 1; i <= 100; i++ ){
+            let elemento = nuovoElemento();
+            elemento.style.width="calc(10% - 10px)";
+            elemento.innerHTML = i;
+            container.append(elemento);
+             
+            elemento.addEventListener("click",
+             function(){
+                elemento.classList.toggle("color");
+                
+             }
+            
+            );
+            
+        }  
+      
+   
+    }
+    
+    else if (difficolta === "Medio"){
+        document.getElementById("container").innerHTML = ""
+        for(i = 1; i <= 81; i++ ){
+            let elemento = nuovoElemento();
+            elemento.style.width="calc(11% - 10px)";
+            elemento.innerHTML = i;
+            container.append(elemento);
+             
+            elemento.addEventListener("click",
+             function(){
+                elemento.classList.toggle("color");
+
+             }
+            
+            );
+             
+        }         
+        
+    }
+
+    else if (difficolta === "Difficile"){
+        document.getElementById("container").innerHTML = ""
+        for(i = 1; i <= 49; i++ ){
+            let elemento = nuovoElemento();
+            elemento.style.width="calc(13% - 10px)";
+            elemento.innerHTML = i;
+            container.append(elemento);
+             
+            elemento.addEventListener("click",
+             function(){
+                elemento.classList.toggle("color");
+             }
+            
+            );
+    
+        }  
+        
+    }
  }
 
  );
+ 
 
-
- function nuovoElemento(){
+function nuovoElemento(){
      let nElemento = document.createElement("div");
      nElemento.classList.add("box");
      return nElemento;
- }
+}
+
+
