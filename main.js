c10f952cb649d2676c7ae7ed376a052f883d3db7
Ibrahim <ibrachar68@gmail.com>
2022-11-16 13:42:17 +0100

    griglia con aggiunta tasto

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 let button = document.getElementById("btn");
-let container=document.getElementById("container");
-for(i=1;i<=100;i++){
-    let elemento= nuovoElemento();
-     elemento.innerHTML=i;
-     elemento.addEventListener("click",
-     function(){
-        elemento.classList.toggle("color")
-     }
-     
-     );
 
-     container.append(elemento);
-}
+ button.addEventListener("click",
+ function(){
+     let container = document.getElementById("container");
+     container.style.backgroundColor = "aqua";
+
+     for(i = 1; i <= 100; i++ ){
+         let elemento = nuovoElemento();
+          elemento.innerHTML = i;
+          container.append(elemento);
+          
+          elemento.addEventListener("click",
+          function(){
+             elemento.classList.toggle("color")
+          }
+         
+          );
+         
+     }  
+ }
+
+ );
 
 
-function nuovoElemento(){
-    let nElemento=document.createElement("div");
-    nElemento.classList.add("box");
-    return nElemento;
-}
+ function nuovoElemento(){
+     let nElemento = document.createElement("div");
+     nElemento.classList.add("box");
+     return nElemento;
+ }
